5f9dbbd8261521b0f7ceb01087ae76bf909e93c5
Matime Bruno <47345325+MatimeBruno@users.noreply.github.com>
2021-10-07 11:59:40 +0400

    little to-do-list
    A made a little to-do-list

--- to-do-list.php
+++ to-do-list.php
@@ -1,4 +1,19 @@
-<?php
-    $message = 'hello';
-    echo $message;
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>To-do-list</h1>
+    <ul>
+        <?php
+            $taskArray = array("Faire les courses", "Faire le ménage", "Faire du sport");
+            foreach($taskArray as $taskValue)
+            {
+                echo '<li>'.$taskValue.'</li>';
+            }
+        ?>
+    </ul>
+</body>
+</html>
